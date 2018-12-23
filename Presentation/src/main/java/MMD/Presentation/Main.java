caa40991bfdd1aa67faa18df1be0e881eda67fd9
Nicu Gorea <nicug99@gmail.com>
2018-12-23 14:35:57 +0200

    Modularization

---
+++ Presentation/src/main/java/MMD/Presentation/Main.java
@@ -0,0 +1,9 @@
+package MMD.Presentation;
+
+public class Main
+{
+    public static void start()
+    {
+	System.out.println("Presentation");
+    }
+}
--- src/main/java/MMDesktop/MovieManagerDesktop/App.java
+++ src/main/java/MMDesktop/MovieManagerDesktop/App.java
@@ -1,13 +1,17 @@
 package MMDesktop.MovieManagerDesktop;
 
+import MMD.Presentation.Main;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
-    public static void main( String[] args )
+    public static void main(final String[] args)
     {
-        System.out.println( "Hello World!" );
+	System.out.println("Hello World!");
+	Main.start();
     }
+
 }
